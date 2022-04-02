3b7617694693218f838780574c5478cb5f481475
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-04-02 22:52:10 +0800

    Update exercise1.c

--- exercise1.c
+++ exercise1.c
@@ -9,11 +9,17 @@
 
 //main function section 
 int main(){
+    
+    float currencyWorld[5] = {4.50, 5.55, 3.65, 1.20, 6.56};
+ 
+    
     //write here your program
-
+    
+    scanf(" %f", &currencyWorld[2]);
+    printf(" %f", currencyWorld[2]);
+    
 
     return 0;
 }
-
 
 //user-defined section 
