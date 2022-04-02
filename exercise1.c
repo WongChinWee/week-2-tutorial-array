26f625be98dc796cb2c9b6234c64ff5e52485505
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-04-02 23:56:39 +0800

    Update exercise1.c

--- exercise1.c
+++ exercise1.c
@@ -9,7 +9,23 @@
 
 //main function section 
 int main(){
+    
+    float currencyWorld[5];
+
+
+    //declare and initialize variable
+    
+    currencyWorld[0] = 4.50;
+    currencyWorld[1] = 5.55;
+    currencyWorld[2] = 3.65;
+    currencyWorld[3] = 1.20;
+    currencyWorld[4] = 6.56;
+
+    
     //write here your program
+
+    printf(" %.2f", currencyWorld[2]);
+
 
 
     return 0;
