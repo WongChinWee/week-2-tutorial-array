819cec9b80a32c1eb8ded9ffd4d469c245049748
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-04-03 22:19:09 +0800

    Update exercise3.c

--- exercise3.c
+++ exercise3.c
@@ -9,7 +9,15 @@
 
 //main function section 
 int main(){
+    
     //write here your program
+
+    char myAlphabet[3][3] = {{h,g,f},{c,n,p},{o,m,q}};
+
+    scanf(" %c", theAlphabet[3][3]);
+
+    printf("Character from Row 3 and Column 3 is %c", theAlphabet[3][3]);
+
 
 
     return 0;
