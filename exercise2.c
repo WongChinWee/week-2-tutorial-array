cc517765ba38ed66a330300ad6466eb06004f0e3
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-04-03 22:09:16 +0800

    Update exercise2.c

--- exercise2.c
+++ exercise2.c
@@ -9,11 +9,18 @@
 
 //main function section 
 int main(){
+    char myName[1] = 'Wong';
+    
+    
     //write here your program
+
+    printf("My Name is %c, Bachelor of Information Technology from Universiti Malaysia Kelantan", myName[1]);
+    
 
 
     return 0;
 }
 
 
-//user-defined section 
+//user-defined section
+
