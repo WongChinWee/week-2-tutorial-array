7bfddd2990d2f0eda7e61d1e7fe39d286b235fd7
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-04-04 14:07:26 +0800

    Update exercise3.c

--- exercise3.c
+++ exercise3.c
@@ -9,11 +9,17 @@
 
 //main function section 
 int main(){
+    
     //write here your program
+
+    char myAlphabet[3][3] = {{'h','g','f'},{'c','n','p'},{'o','m','q'}};
+
+    printf("Character from Row 3 and Column 3 is %c", myAlphabet[2][2]);
+
 
 
     return 0;
 }
 
 
-//user-defined section 
+//user-defined section
