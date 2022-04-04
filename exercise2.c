ab5ee7221b436687dd3765aed2d2333b7b124f08
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-04-04 14:09:18 +0800

    Update exercise2.c
    //documentation section 
    /* Exercise 2 - 1D array */
    
    //pre-processor section 
    #include<stdio.h>
    
    //global variable section 
    
    
    //main function section 
    int main(){
        char myName[100] = {"Wong"};
        
        
        //write here your program
    
        printf("My Name is %s, Bachelor of Information Technology from Universiti Malaysia Kelantan", myName);
        
    
    
        return 0;
    }
    
    
    //user-defined section

--- exercise2.c
+++ exercise2.c
@@ -9,11 +9,18 @@
 
 //main function section 
 int main(){
+    char myName[100] = {"Wong"};
+    
+    
     //write here your program
+
+    printf("My Name is %s, Bachelor of Information Technology from Universiti Malaysia Kelantan", myName);
+    
 
 
     return 0;
 }
 
 
-//user-defined section 
+//user-defined section
+
