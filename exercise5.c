25e20a28f0dcf200fc18c0a7713c065dc6960ba7
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-04-04 14:03:28 +0800

    Update exercise5.c

--- exercise5.c
+++ exercise5.c
@@ -11,9 +11,25 @@
 int main(){
     //write here your program
 
+    int myArray[5];
+    int i;
+    int averageVal;
+    int sum = 0;
+
+    for (i=0; i<5; i++){
+
+        printf("Enter Value: ");
+        scanf(" %d", &myArray[i]);
+        sum = sum + myArray[i];
+
+    }
+
+    averageVal = sum / 5;
+
+    printf("Average value is: %d", averageVal);
+
 
     return 0;
 }
-
 
 //user-defined section 
