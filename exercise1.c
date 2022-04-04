1e52fc458d1a770b78b6e548ce58ff89d3af83af
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-04-04 14:10:33 +0800

    Update exercise1.c
    //documentation section 
    /* Exercise 1 - 1D array */
    
    //pre-processor section 
    #include<stdio.h>
    
    //global variable section 
    
    
    //main function section 
    int main(){
    
        float currencyWorld[5];
    
    
        //declare and initialize variable
        
        currencyWorld[0] = 4.50;
        currencyWorld[1] = 5.55;
        currencyWorld[2] = 3.65;
        currencyWorld[3] = 1.20;
        currencyWorld[4] = 6.56;
    
        
        //write here your program
    
        printf(" %.2f", currencyWorld[2]);
    
    
        return 0;
         
    
    
    }
    
    
    //user-defined section

--- exercise1.c
+++ exercise1.c
@@ -9,11 +9,29 @@
 
 //main function section 
 int main(){
+
+    float currencyWorld[5];
+
+
+    //declare and initialize variable
+    
+    currencyWorld[0] = 4.50;
+    currencyWorld[1] = 5.55;
+    currencyWorld[2] = 3.65;
+    currencyWorld[3] = 1.20;
+    currencyWorld[4] = 6.56;
+
+    
     //write here your program
+
+    printf(" %.2f", currencyWorld[2]);
 
 
     return 0;
+    
+  
+
 }
 
 
-//user-defined section 
+//user-defined section
